4095c36105dc074f0a8f6a39e9fdc604ab021b9a
drmoore718 <drmoore718@gmail.com>
2023-12-16 12:44:01 -0800

    switch back to recursive, switch to bytes, switch to 1d vecs

--- day16/src/part2_fast.rs
+++ day16/src/part2_fast.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use num::integer::Roots;
 use rayon::prelude::*;
 
 #[derive(Copy, Clone)]
@@ -11,190 +12,186 @@ enum FromDirection {
 
 pub fn execute(input: &str) -> String {
     let inputs = parseInput(input.trim());
+    let len = inputs.len().sqrt();
     // println!("{:#?}", inputs);
+    // println!("{}", len);
 
     static DIRECTIONS: [FromDirection; 4] = [FromDirection::Left, FromDirection::Right, FromDirection::Top, FromDirection::Bottom];
-    (0..inputs[0].len()).cartesian_product(DIRECTIONS.iter()).par_bridge().map(|(idx, dir)| {
-        let mut energized = vec![vec![false; inputs[0].len()]; inputs.len()];
-        let mut visited: Vec<Vec<bool>> = vec![vec![false; inputs[0].len()]; inputs.len()];
+    (0..len).cartesian_product(DIRECTIONS.iter()).par_bridge().map(|(idx, dir)| {
+        let mut energized = vec![false; len*len];
+        let mut visited = vec![false; len*len];
 
         match dir {
-            FromDirection::Left => traverse(&inputs, 0, idx, *dir, &mut energized, &mut visited),
-            FromDirection::Right => traverse(&inputs, inputs[0].len() - 1, idx, *dir, &mut energized, &mut visited),
-            FromDirection::Top => traverse(&inputs, idx, 0, *dir, &mut energized, &mut visited),
-            FromDirection::Bottom => traverse(&inputs, idx, inputs.len() - 1, *dir, &mut energized, &mut visited),
+            FromDirection::Left => traverse(&inputs, 0, idx, *dir, &mut energized, &mut visited, len),
+            FromDirection::Right => traverse(&inputs, len - 1, idx, *dir, &mut energized, &mut visited, len),
+            FromDirection::Top => traverse(&inputs, idx, 0, *dir, &mut energized, &mut visited, len),
+            FromDirection::Bottom => traverse(&inputs, idx, len - 1, *dir, &mut energized, &mut visited, len),
         }
 
-        energized.iter().flatten().filter(|x| **x).count()
+        energized.iter().filter(|x| **x).count()
     }).reduce(|| 0, |a,b| a.max(b)).to_string()
 }
 
-fn traverse(grid: &Vec<Vec<char>>, x_s:usize, y_s:usize, dir_s: FromDirection, energized: &mut Vec<Vec<bool>>, visited: &mut Vec<Vec<bool>>) {
+fn traverse(grid: &[u8], x:usize, y:usize, dir: FromDirection, energized: &mut Vec<bool>, visited: &mut Vec<bool>, len: usize) {
+    let idx = y * len + x;
+    energized[idx] = true;
 
-    let mut q: Vec<(usize, usize, FromDirection)> = Default::default();
+    match dir {
+        FromDirection::Left => {
+            match grid[idx] {
+                b'\\' => {
+                    if y+1 < len {
+                        traverse(grid, x, y+1, FromDirection::Top, energized, visited, len);
+                    }
+                },
+                b'/' => {
+                    if y > 0 {
+                        traverse(grid, x, y-1, FromDirection::Bottom, energized, visited, len);
+                    }
+                },
+                b'|' => {
+                    if visited[idx] {
+                        return;
+                    }
+                    visited[idx] = true;
 
-    q.push((x_s, y_s, dir_s));
-
-    while !q.is_empty() {
-        let (x,y,dir) = q.pop().unwrap();
-        energized[y][x] = true;
-
-        match dir {
-            FromDirection::Left => {
-                match grid[y][x] {
-                    '\\' => {
-                        if y+1 < grid.len() {
-                            q.push((x, y+1, FromDirection::Top));
-                        }
-                    },
-                    '/' => {
-                        if y > 0 {
-                            q.push((x, y-1, FromDirection::Bottom));
-                        }
-                    },
-                    '|' => {
-                        if visited[y][x] {
-                            continue;
-                        }
-                        visited[y][x] = true;
-
-                        if y+1 < grid.len() {
-                            q.push((x, y+1, FromDirection::Top));
-                        }
-                        if y > 0 {
-                            q.push((x, y-1, FromDirection::Bottom));
-                        }
-                    },
-                    '-' => {
-                        if x+1 < grid[0].len() {
-                            q.push((x+1, y, FromDirection::Left));
-                        }
-                    },
-                    '.' => {
-                        if x+1 < grid[0].len() {
-                            q.push((x+1, y, FromDirection::Left));
-                        }
-                    },
-                    _ => panic!(),
-                }
-            },
-            FromDirection::Right => {
-                match grid[y][x] {
-                    '\\' => {
-                        if y > 0 {
-                            q.push((x, y-1, FromDirection::Bottom));
-                        }
-                    },
-                    '/' => {
-                        if y+1 < grid.len() {
-                            q.push((x, y+1, FromDirection::Top));
-                        }
-                    },
-                    '|' => {
-                        if visited[y][x] {
-                            continue;
-                        }
-                        visited[y][x] = true;
-                        if y+1 < grid.len() {
-                            q.push((x, y+1, FromDirection::Top));
-                        }
-                        if y > 0 {
-                            q.push((x, y-1, FromDirection::Bottom));
-                        }
-                    },
-                    '-' => {
-                        if x > 0 {
-                            q.push((x-1, y, FromDirection::Right));
-                        }
-                    },
-                    '.' => {
-                        if x > 0 {
-                            q.push((x-1, y, FromDirection::Right));
-                        }
-                    },
-                    _ => panic!(),
-                }
-            },
-            FromDirection::Top => {
-                match grid[y][x] {
-                    '\\' => {
-                        if x+1 < grid[0].len() {
-                            q.push((x+1, y, FromDirection::Left));
-                        }
-                    },
-                    '/' => {
-                        if x > 0 {
-                            q.push((x-1, y, FromDirection::Right));
-                        }
-                    },
-                    '|' => {
-                        if y+1 < grid.len() {
-                            q.push((x, y+1, FromDirection::Top));
-                        }
-                    },
-                    '-' => {
-                        if visited[y][x] {
-                            continue;
-                        }
-                        visited[y][x] = true;
-                        if x > 0 {
-                            q.push((x-1, y, FromDirection::Right));
-                        }
-                        if x+1 < grid[0].len() {
-                            q.push((x+1, y, FromDirection::Left));
-                        }
-                    },
-                    '.' => {
-                        if y+1 < grid.len() {
-                            q.push((x, y+1, FromDirection::Top));
-                        }
-                    },
-                    _ => panic!(),
-                }
-            },
-            FromDirection::Bottom => {
-                match grid[y][x] {
-                    '\\' => {
-                        if x > 0 {
-                            q.push((x-1, y, FromDirection::Right));
-                        }
-                    },
-                    '/' => {
-                        if x+1 < grid[0].len() {
-                            q.push((x+1, y, FromDirection::Left));
-                        }
-                    },
-                    '|' => {
-                        if y > 0 {
-                            q.push((x, y-1, FromDirection::Bottom));
-                        }
-                    },
-                    '-' => {
-                        if visited[y][x] {
-                            continue;
-                        }
-                        visited[y][x] = true;
-                        if x > 0 {
-                            q.push((x-1, y, FromDirection::Right));
-                        }
-                        if x+1 < grid[0].len() {
-                            q.push((x+1, y, FromDirection::Left));
-                        }
-                    },
-                    '.' => {
-                        if y > 0 {
-                            q.push((x, y-1, FromDirection::Bottom));
-                        }
-                    },
-                    _ => panic!(),
-                }
-            },
-        }
+                    if y+1 < len {
+                        traverse(grid, x, y+1, FromDirection::Top, energized, visited, len);
+                    }
+                    if y > 0 {
+                        traverse(grid, x, y-1, FromDirection::Bottom, energized, visited, len);
+                    }
+                },
+                b'-' => {
+                    if x+1 < len {
+                        traverse(grid, x+1, y, FromDirection::Left, energized, visited, len);
+                    }
+                },
+                b'.' => {
+                    if x+1 < len {
+                        traverse(grid, x+1, y, FromDirection::Left, energized, visited, len);
+                    }
+                },
+                _ => panic!(),
+            }
+        },
+        FromDirection::Right => {
+            match grid[idx] {
+                b'\\' => {
+                    if y > 0 {
+                        traverse(grid, x, y-1, FromDirection::Bottom, energized, visited, len);
+                    }
+                },
+                b'/' => {
+                    if y+1 < len {
+                        traverse(grid, x, y+1, FromDirection::Top, energized, visited, len);
+                    }
+                },
+                b'|' => {
+                    if visited[idx] {
+                        return;
+                    }
+                    visited[idx] = true;
+                    if y+1 < len {
+                        traverse(grid, x, y+1, FromDirection::Top, energized, visited, len);
+                    }
+                    if y > 0 {
+                        traverse(grid, x, y-1, FromDirection::Bottom, energized, visited, len);
+                    }
+                },
+                b'-' => {
+                    if x > 0 {
+                        traverse(grid, x-1, y, FromDirection::Right, energized, visited, len);
+                    }
+                },
+                b'.' => {
+                    if x > 0 {
+                        traverse(grid, x-1, y, FromDirection::Right, energized, visited, len);
+                    }
+                },
+                _ => panic!(),
+            }
+        },
+        FromDirection::Top => {
+            match grid[idx] {
+                b'\\' => {
+                    if x+1 < len {
+                        traverse(grid, x+1, y, FromDirection::Left, energized, visited, len);
+                    }
+                },
+                b'/' => {
+                    if x > 0 {
+                        traverse(grid, x-1, y, FromDirection::Right, energized, visited, len);
+                    }
+                },
+                b'|' => {
+                    if y+1 < len {
+                        traverse(grid, x, y+1, FromDirection::Top, energized, visited, len);
+                    }
+                },
+                b'-' => {
+                    if visited[idx] {
+                        return;
+                    }
+                    visited[idx] = true;
+                    if x > 0 {
+                        traverse(grid, x-1, y, FromDirection::Right, energized, visited, len);
+                    }
+                    if x+1 < len {
+                        traverse(grid, x+1, y, FromDirection::Left, energized, visited, len);
+                    }
+                },
+                b'.' => {
+                    if y+1 < len {
+                        traverse(grid, x, y+1, FromDirection::Top, energized, visited, len);
+                    }
+                },
+                _ => panic!(),
+            }
+        },
+        FromDirection::Bottom => {
+            match grid[idx] {
+                b'\\' => {
+                    if x > 0 {
+                        traverse(grid, x-1, y, FromDirection::Right, energized, visited, len);
+                    }
+                },
+                b'/' => {
+                    if x+1 < len {
+                        traverse(grid, x+1, y, FromDirection::Left, energized, visited, len);
+                    }
+                },
+                b'|' => {
+                    if y > 0 {
+                        traverse(grid, x, y-1, FromDirection::Bottom, energized, visited, len);
+                    }
+                },
+                b'-' => {
+                    if visited[idx] {
+                        return;
+                    }
+                    visited[idx] = true;
+                    if x > 0 {
+                        traverse(grid, x-1, y, FromDirection::Right, energized, visited, len);
+                    }
+                    if x+1 < len {
+                        traverse(grid, x+1, y, FromDirection::Left, energized, visited, len);
+                    }
+                },
+                b'.' => {
+                    if y > 0 {
+                        traverse(grid, x, y-1, FromDirection::Bottom, energized, visited, len);
+                    }
+                },
+                _ => panic!(),
+            }
+        },
     }
+
 }
 
-fn parseInput(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|x| x.chars().collect()).collect()
+fn parseInput(input: &str) -> Vec<u8> {
+    input.lines().flat_map(|x| x.bytes()).collect()
 }
 
 #[cfg(test)]
